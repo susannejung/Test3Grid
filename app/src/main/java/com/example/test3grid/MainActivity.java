a6d2c0e70919596725ba94571a074ec5b83adcd2
susannejung <95688233+susannejung@users.noreply.github.com>
2022-05-01 09:57:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/test3grid/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.test3grid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+import android.os.Bundle;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.GridView;
+        import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GridView gv = (GridView) findViewById(R.id.gridview);
+        gv.setAdapter(new ImageAdapter(this));
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(MainActivity.this, "Image Position: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
